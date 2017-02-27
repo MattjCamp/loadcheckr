0aab832c96d1252a4b3bb425a15da81b4db00d50
mattjcamp <mjcampbell74@gmail.com>
2017-02-27 08:52:43 -0500

    Verified package builds

--- R/check_db_rows.R
+++ R/check_db_rows.R
@@ -1,4 +1,13 @@
 
+#' Check Row Counts
+#'
+#' Checks the number of rows in both data tables
+#' @param conn_a the first dbr data connection
+#' @param table_a the first table name
+#' @param conn_b the second dbr data connection
+#' @param table_b the second table name
+#' @export
+#' @return a list of objects describing the results, prints out to the log as well
 
 check_db_rows <- function(conn_a, table_, conn_b, table_b) {
 
